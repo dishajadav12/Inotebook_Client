16ababfbb123d85952bea636cc17aeb7649c5b22
Disha <dishajadav12402@gmail.com>
2023-07-09 15:53:40 +0530

    setup routes and basic components for iNotebook

---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>iNotebook - Your digital notebook</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">iNotebook</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className='navbar-nav me-auto mb-2 mb-lg-0'>
+        <div className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+        </div>
+        <div className="nav-item">
+          <Link className="nav-link" to="/about">About</Link>
+        </div>
+        </div>
+        <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
